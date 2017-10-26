08838b038e2cd9962e01e91414e8bb6a291514ba
Daire Ni Chathain <dairenichat@gmail.com>
2017-10-26 17:05:44 +0100

    corrected formatting

--- 05_check_cookie.go
+++ 05_check_cookie.go
@@ -1,5 +1,5 @@
 //Author : Daire Ni Chathain
-//Go exercise 4 : Guessing game with Bootstrap
+//Go exercise 5 : Guessing game with Bootstrap + check for cookie
 //Resources:
 // 1) http://goinbigdata.com/example-of-using-templates-in-golang/
 // 2) https://stackoverflow.com/questions/26559557/how-do-you-serve-a-static-html-file-using-a-go-web-server
@@ -10,20 +10,35 @@ package main
 import (
     "html/template"
 	"net/http"
+	"strconv"
+	"math/rand"
+	"time"
 )
-//Messgage struct that hold values that will be parsed & rendered using the guess.tmpl
+
 type message struct {
 	Message string
+//	Header string
 }
 
-// when request come in it goes to this function first
+// the handler function that gets executed every time a request arrives at the root
 func requestHandler(w http.ResponseWriter, r *http.Request) {
-	//serve the index file on load
 	http.ServeFile(w, r, "index.html")
 }
 
 //the handler that gets executed when a request to /guess is made
 func gameHandler(w http.ResponseWriter, r *http.Request) {
+
+		//random num generator
+		rand.Seed(time.Now().UTC().UnixNano())
+
+		//First check if a cookie exists already * name it target
+		cookie, err := r.Cookie("target")
+		if err != nil {
+			//if no cookie, set one with random num between 1 and 20
+			http.SetCookie(w, &http.Cookie{Name: "target", Value: strconv.Itoa(rand.Intn(19) + 1)})
+			
+		}
+
 	// Set Message
 	msg := message{Message: "Guess a number between 1 & 20!"}
 	// Guess template file that will display the message struct
